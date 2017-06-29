15ceb41be5b25736ca5d2dc1a77b60f5673a1921
deq2000 <inazu2014@gmail.com>
2017-06-29 23:22:50 +0000

    Jaden and Shemar did work. Forgot to fork so I copied our work to this project

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+/* global $ */
 $(document).ready(function() {
-
-   
     
+  $("#signUp").click(function() {
+      alert("Hi! Thank you for signing up! We will send you an email shortly!");
+  });
+   
+  
+  
+  $(".media").mouseenter(function(){
+    $(this).stop(true,true).animate({height: "75px", width: "75px"}, "slow");
+	}).mouseleave(function(){
+	  $(this).stop(true,true).animate({height: "50px", width: "50px"}, "slow");
+	})
+  
+
 
 });
